1e5194648234526937b60dae838e9ad5d06e3df0
kombton <114578070+kombton@users.noreply.github.com>
2023-12-12 22:44:56 +0100

    h

--- autoplay.js
+++ autoplay.js
@@ -1,40 +1,64 @@
-// script.js
-
 var video1 = document.getElementById("video1");
 var video2 = document.getElementById("video2");
 var clickMessage = document.getElementById("clickMessage");
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
-// Display the click message only for Chrome users
 if (isChrome) {
+    // Display the click message for Chrome users
     clickMessage.style.display = 'block';
 
-    document.addEventListener('click', function(event) {
+    // For Chrome, add a click event listener to execute playFirstVideo on user interaction
+    var clickHandler = function(event) {
+        // Execute the playFirstVideo function when the user clicks anywhere on the page
         playFirstVideo();
-    });
+
+        // Remove the click event listener after the first click
+        document.removeEventListener('click', clickHandler);
+    };
+
+    document.addEventListener('click', clickHandler);
 }
 
+video1.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+});
+
+video2.addEventListener('contextmenu', function (event) {
+    event.preventDefault();
+});
+
 function playFirstVideo() {
+    // Hide the second video and display the first one
     video2.style.display = 'none';
     video1.style.display = 'block';
+
+    // Play the first video
     video1.play();
 
+    // Add an event listener to video1 to execute playSecondVideo when it ends
     video1.addEventListener('ended', playSecondVideo);
 
+    // Hide the click message after the user clicks
     clickMessage.style.display = 'none';
 }
 
 function playSecondVideo() {
+    // Hide the first video and display the second one
     video1.style.display = 'none';
     video2.style.display = 'block';
+
+    // Play the second video
     video2.play();
 
+    // Add an event listener to video2 to loop it when it ends
     video2.addEventListener('ended', function() {
+        // Replay the second video
         video2.currentTime = 0;
         video2.play();
     });
 }
 
+// Add your code from video.js here (assuming it doesn't have conflicting variable names or functions)
 function playSecondVideo() {
     document.getElementById('video1').style.display = 'none';
     document.getElementById('video2').style.display = 'block';
